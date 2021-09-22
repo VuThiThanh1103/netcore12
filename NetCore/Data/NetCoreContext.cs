c7fdf0ac691a2fbcd110476a42506e9104959338
VuThiThanh1103 <vuthanh2906@gmail.com>
2021-09-22 18:15:02 +0700

    first commit

---
+++ NetCore/Data/NetCoreContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using NetCore.Models;
+
+namespace NetCore.Data
+{
+    public class NetCoreContext : DbContext
+    {
+        public NetCoreContext (DbContextOptions<NetCoreContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<NetCore.Models.Movie> Movie { get; set; }
+    }
+}
